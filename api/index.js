0042d90c1ac3f28f0fe6893b4875ed208e458c30
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2024-04-01 21:44:58 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors")
 const authRouter = require("./routes/user.routes");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 // configuration for .env file
 dotenv.config();
@@ -11,9 +13,32 @@ const app = express();
 
 // middleware to parse the json object
 app.use(express.json())
+app.use(express.static('public'));
+
 
 // middleware for cross origin resource sharing
 app.use(cors())
+
+const options = {
+    definition : {
+      openapi : '3.0.0',
+      info : {
+        title : 'Documentation : Entertainment App',
+        version : "1.0"
+      },
+      servers : [
+        {
+          url : "https://fullstack-movie-app.onrender.com/",
+        }
+      ]
+    },
+    apis : ["./routes/user.routes.js"]
+  }
+  
+  const swaggerSpec = swaggerJSDoc(options)
+  console.log(JSON.stringify(swaggerSpec, null, 2));
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // conecting the database with mongoose
 mongoose.connect(process.env.MONGO_URI).then(() => {
